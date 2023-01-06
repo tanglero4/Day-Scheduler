dbea24a572ffd70363fccf71e1f58b496953a356
tanglero4 <tiffanyanglero94@hotmail.com>
2023-01-06 18:52:58 -0500

    Added local storage in js to store user input from text area in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,21 @@
 // in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  $(".saveBtn").on("click", function (params) {
+    console.log(this);
+    var id = $(this).attr("id");
+
+    //get user input from textarea
+    const userValue = document.getElementById("user-input").value;
+
+    //sets up local storage or make an array to put the user input
+    let toDo = JSON.parse(localStorage.getItem("To do")) || [];
+
+    // save the users input to local storage
+    toDo.push(userValue);
+    localStorage.setItem("To Do", JSON.stringify(toDo));
+  });
+
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
